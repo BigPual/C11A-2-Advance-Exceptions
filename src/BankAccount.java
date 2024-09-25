d581c8c01b7ab3d7c692aaeeeca1b7c46b0286ec
Paul Russell <pcrussell03@gmail.com>
2024-09-25 13:35:21 -0600

    Advance Exceptions V2

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,23 @@
 
 public class BankAccount {
 
+	
+	private double balance;
+	
+	public BankAccount(double b)
+	{
+		balance = b;
+	}
+	
+	public void withdraw(double w) throws NegativeBalanceException
+	{
+		if(w > balance)
+		{
+			throw new NegativeBalanceException(balance-w);
+		}
+		else
+		{
+			balance -= w;
+		}
+	}
 }
--- src/NegativeBalanceException.java
+++ src/NegativeBalanceException.java
@@ -1,3 +1,4 @@
+import java.io.PrintWriter;
 
 public class NegativeBalanceException extends Exception{
 	
@@ -5,14 +6,28 @@ public class NegativeBalanceException extends Exception{
 	
 	public NegativeBalanceException()
 	{
-		super();
+		super("Error: negative balance");
 		
 	}
 	
 	public NegativeBalanceException(double d)
 	{
-		super();
+		super("Amount exceeds balance by " + d);
 		negativeBalance = d;
+		try
+		{
+			PrintWriter writer = new PrintWriter("logfile.txt");
+			writer.println("Amount exceeds balance by " + d);
+			writer.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Log file failed to open");		}
+	}
+
+	@Override
+	public String toString() {
+		return "Balance of " + negativeBalance + " not allowed";
 	}
 
 }
